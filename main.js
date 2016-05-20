1175c1bd35688866aebc6bcdbc965048a23e5e05
Kevin Lacker <lacker@gmail.com>
2016-05-19 20:56:40 -0700

    configure firebase

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
+const firebase = require('firebase');
+
+const config = {
+  apiKey: "AIzaSyBdcfWpyvzuSEyS03M6RAIcbneQKkINOHw",
+  authDomain: "project-8181368540383796399.firebaseapp.com",
+  databaseURL: "https://project-8181368540383796399.firebaseio.com",
+  storageBucket: "project-8181368540383796399.appspot.com",
+};
+firebase.initializeApp(config);
+
+
 // Listen to stdin for local messages
 rl.setPrompt('> ');
 rl.prompt();
